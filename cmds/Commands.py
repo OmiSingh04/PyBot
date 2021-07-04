94288317a1e6944afc367cce8a9542901b284b78
edantuti <66787358+Edantuti@users.noreply.github.com>
2021-07-04 18:31:42 +0530

    Added Comments and Deleted unnecessary spaces

--- cmds/Commands.py
+++ cmds/Commands.py
@@ -3,27 +3,29 @@ from discord import Embed, Color
 class Commands(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
-
+	#Indication for startup
 	@commands.Cog.listener()
 	async def on_ready(self):
 		print("ready")
-
+	
+	#Command for testing
 	@commands.command(name="cmds")
 	async def cmds(self, ctx):
 		await ctx.send('test')
-
+	
+	#Command for pet
 	@commands.command(name='pet', brief="p")
 	async def pet(self, ctx):
 		embed=Embed(title="Woof Woof!!", color=Color.blue())
 		embed.set_image(url="https://forgifs.com/gallery/d/287041-2/Husky-dog-jealous-of-stuffed-animal.gif")
 		await ctx.send(embed=embed)
 	
+	#Command for sleep
 	@commands.command(name='sleep', brief="s")
 	async def sleep(self, ctx):
 		embed=Embed(title="ZZZzzZZzz!!", color=Color.blue())
 		embed.set_image(url="https://media.tenor.com/images/eca1cd1abf2ed496866e7392292cd755/tenor.gif")
 		await ctx.send(embed=embed)
-
 
 def setup(bot):
 	bot.add_cog(Commands(bot))
