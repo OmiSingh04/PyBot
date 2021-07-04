987dfb1d8c8313db904c701e00007a6547a93cce
Edantuti <66787358+Edantuti@users.noreply.github.com>
2021-07-04 17:33:39 +0530

    basic code layout

--- main.py
+++ main.py
@@ -16,3 +16,39 @@ async def cool(ctx):
     await ctx.send("cool cool cool cool cool cool")
 
 client.run(token)
+
+#main file code
+# from discord.ext import commands
+# from discord import Embed, Color, Status
+
+# bot = commands.Bot(command_prefix=commands.when_mentioned_or('#'))
+
+# bot.remove_command('help')
+
+# @bot.event
+# async def on_ready():
+#     await bot.change_presence(status=Status.idle)
+
+# extensions = ['Commands', 'Accesories', 'Shop']
+
+# for cogs in extensions:
+#     bot.load_extension(f'{cogs}')
+
+# bot.run(token)
+
+
+# Commands file code
+# from discord.ext import commands
+# from discord import Embed, Color
+
+# class Command(commands.Cog):
+#     def _init_(self, bot):
+#         self.bot = bot
+
+#     @commands.group(name='commands', invoke_without_command=True)
+#     async def cmdshelp(self, ctx):
+#         await ctx.send('test')
+
+#     @cmdshelp.command(name="synatx")
+#     async def syntax(self, ctx):
+#         await ctx.send('test')
