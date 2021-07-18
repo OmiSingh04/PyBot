ed029afb0b2d5941f1d8711f7ea0ee8f3bfe4b14
OmiSingh2004 <omisinghdbz@yahoo.com>
2021-07-18 23:02:53 +0530

    changes mannn

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from discord.ext import commands
+import discord
 from discord import Embed, Color, Status
 from dotenv import load_dotenv
 from db_manager.db_commands import DbCommands
+from shop.shop_manager import Shop
 import os
 
 load_dotenv()
@@ -11,23 +13,24 @@ bot = commands.Bot(command_prefix=commands.when_mentioned_or('?'))
 
 
 token = os.getenv('TOKEN')
-db_user = os.getenv('DB_USER')
-db_password = os.getenv('DB_PASSWORD')
-db_database = os.getenv('DB_DATABASE')
+#db_user = os.getenv('DB_USER')
+#db_password = os.getenv('DB_PASSWORD')
+#db_database = os.getenv('DB_DATABASE')
 
 bot.remove_command('help')
 
 @bot.event
 async def on_ready():
-    await bot.change_presence(status=Status.user_idle)
+    print("Ready")
+    await bot.change_presence(status=Status.idle, activity = discord.Game(name = "Woofies"))
 
 #List of Cogs
-extensions = ['cmds.Commands']
+extensions = ['cmds.Commands', 'shop.shop_manager']
 
 #initiation of Cogs
 for cogs in extensions:
     bot.load_extension(f'{cogs}')
 
-bot.add_cog(DbCommands(bot, db_user, db_password, db_database))
+#bot.add_cog(DbCommands(bot, db_user, db_password, db_database))
 
 bot.run(token)
