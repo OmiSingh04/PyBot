1885a4b2519382885e3d77205ae1dbcccde184f5
OmiSingh2004 <omisinghdbz@gmail.com>
2021-08-21 14:21:59 +0530

    a game like design for the pet

--- model/pet.py
+++ model/pet.py
@@ -1,6 +1,21 @@
+import pygame
 class Pet:
-    def __init__():
-        pass
+    def __init__(self, name, health, ability):
+        self.name = name
+        self.health = health
+        self.ability = ability
+        self.hunger = 0
+        self.clock = pygame.time.Clock()
 
-    def bruh():
-        pass
+
+    def tick(self):
+        hunger+=5
+
+    def feed(self,amount)
+        self.hunger -= amount
+
+    def start(self):
+        self.running = True
+        while running:
+            tick()
+            clock.tick(1/60)
